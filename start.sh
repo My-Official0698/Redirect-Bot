19751b4216080ea85429c15d960cbc1e01028e31
DesiBots <103522724+DesiBots@users.noreply.github.com>
2022-05-22 11:27:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/repo.git /repo
+  git clone https://github.com/DesiBots/Redirect-Bot.git /repo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /repo
 fi
 cd /repo
 pip3 install -U -r requirements.txt
-echo "Starting SS_Linkz Botx...."
+echo "Starting Kylie Bot🔥...."
 python3 bot.py
